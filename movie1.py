5809df0a391489dbe7b81858662b0103e514e99e
enni20170711-boop <enni20170711@gmail.com>
2026-04-25 23:49:34 +0800

    Add files via upload

--- movie1.py
+++ movie1.py
@@ -7,7 +7,7 @@ Data.encoding = "utf-8"
 #print(Data.text)
 sp = BeautifulSoup(Data.text, "html.parser")
 result=sp.select("filmListAllX li")
-q = input("請輸入片名關鍵字")
+q = input("請輸入片名關鍵字:")
 for item in result:
 	if q in item.find("img").get("alt"):
 	print(item.fnd("img").get("alt")
