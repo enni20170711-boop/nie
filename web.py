effd9a2c914d599d8860d7796c88d61d56a0e59e
enni20170711-boop <enni20170711@gmail.com>
2026-05-14 11:05:28 +0800

    webhook

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ else:
 firebase_admin.initialize_app(cred)
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response, jsonify
 from datetime import datetime
 import random
 
@@ -29,6 +29,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
 
 @app.route("/rate")
 def rate():
